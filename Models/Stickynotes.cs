1aaa5ea84bec3e69aabbe958934948091c7094cd
Lena Kinney <lenakinney@Lenas-MacBook-Pro.local>
2020-11-19 11:22:16 -0500

    model and planning updated

---
+++ Models/Stickynotes.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace stickynotes.Models{
+
+public class Sticky
+{
+
+    public int Id {get;set;}
+        public string Notetitle {get;set;} 
+        public string Notetext {get;set;}
+        public int Order {get;set;}
+
+        [DataType(DataType.Date)]
+        public DateTime NoteDate { get; set; }
+
+}
+}
